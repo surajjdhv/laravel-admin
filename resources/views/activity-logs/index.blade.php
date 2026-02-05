718797867adef08c79605fb216813ffe633b55e6
Suraj Jadhav <mail@surajjadhav.me>
2026-02-05 03:55:04 +0000

    Standerdise new table ui's

--- resources/views/activity-logs/index.blade.php
+++ resources/views/activity-logs/index.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="card-body">
         <div class="table-responsive">
-            <table class="table table-striped align-middle">
+            <table class="table table-striped table-borderless align-middle">
                 <thead>
                     <tr>
                         <th>When</th>
